ea9273fff89a4ed499f91b39c63af8d741f35ca4
Andrew Kaiser <kaisea.rpi@gmail.com>
2024-03-05 08:48:02 -0500

    api declaration suggestions

--- tests/ts-rpc.test.ts
+++ tests/ts-rpc.test.ts
@@ -2,7 +2,7 @@ import { test } from '../../testing/tools.ts'
 
 import {create_rpc_server} from '../server.ts'
 import {create_rpc_client} from '../client.ts'
-import {oak} from '../src/deps.server.ts'
+import {z, oak} from '../src/deps.server.ts'
 
 test('basic', async t => {
   const app = new oak.Application()
@@ -19,3 +19,40 @@ test('basic', async t => {
   server_controller.abort()
   await server_promise
 })
+
+
+const User = z.object({
+  id: z.string(),
+  username: z.string(),
+  created_at: z.datetime(),
+})
+
+const api = create_api({
+  accounts: create_module({
+    // design #1
+    create: create_function({
+      params: z.object({ username: z.string() }),
+      result: User
+    })
+
+    // design #2
+    create: create_function(
+      z.object({ username: z.string() }),
+      User,
+    )
+
+    // design #3
+    create: z.function()
+      .args([z.object({ username: z.string() })])
+      .returns(User)
+  })
+})
+
+// NOTE if we want to create a server function (validate args only, and make validation optional)
+// and a client side function (validate returns as args) we may need to create our own function() method that dumps enough data to create those zod validators
+const zfunc = z.function()
+  .args([z.number()])
+  .returns(z.string())
+
+
+
